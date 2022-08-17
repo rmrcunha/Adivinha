4f64108d630043334955b37e346dd40fecf0785f
Renan Cunha <renancdev@gmail.com>
2022-08-17 20:21:35 -0300

    adicionando condicionais ao jogo, bem como o número de tentativas que foram executadas

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,16 +1,48 @@
 #include <stdio.h>
-
+#define NUMERO_DE_TENTATIVAS 3
 
 int main() {
     printf("****************************************\n");
     printf ("*Bem vindo ao nosso jogo de adivinhação*\n");
     printf("****************************************\n");
 
-    int numeroSecreto = 42;
+    int numerosecreto = 42;
 
     int chute;
+    int tentativa = 1;
+    while (1){
 
-    printf("Qual é o seu chute?");
-    scanf("%d", &chute);
-    printf("Seu chute foi %d", chute);
+        printf("Tentativa %d\n", tentativa);
+        printf("Qual é o seu chute?");
+
+        scanf("%d", &chute);
+        printf("Seu chute foi %d", chute);
+
+        if(chute < 0){
+            printf("Você não pode chutar números negativos");
+
+            continue;
+        }
+
+        int acertou = chute == numerosecreto;
+        int maior = chute > numerosecreto;
+
+        if(acertou){
+            printf("Parabéns! Você acertou!\n");
+            printf("Jogue novamente!");
+
+            break;
+        }
+        else if(maior){
+            printf("Seu chute foi maior que o número secreto.\n");
+        }
+
+        else{
+            printf("Seu chute foi menor que o número secreto.\n");
+        }
+        tentativa++;
+    }
+
+    printf("Fim de jogo!\n");
+    printf("Você terminou o jogo em %d tentativas", tentativa-1);
 }
